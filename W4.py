888c9e782225a118c4df9c82997f749b44ff7d33
Alexandra <65454440+alexandrabal@users.noreply.github.com>
2021-01-31 23:03:43 +0000

    Update W4.py

--- W4.py
+++ W4.py
@@ -1,20 +1,28 @@
-# declară o listă care conține elementele 7, 8, 9, 2, 3, 1, 4, 10, 5, 6 (în această ordine).
+# LISTS
+# a list with the following elements 7, 8, 9, 2, 3, 1, 4, 10, 5, 6 (in this order)
 my_list = [7,8,9,2,3,1,4,10,5,6]
-# afișează lista inițială ordonată ascendent (lista inițială trebuie păstrată în aceeași formă)
+
+# print a list in ascendent order
 my_list.sort()
-print(my_list)
-# afișează lista inițială ordonată descendent (lista inițială trebuie păstrată în aceeași formă)
+print("sorted " + str(my_list))
+
+
+# print a list in a descendent order from the highest to the lowest
 my_list.sort(reverse = True)
 print(my_list)
-# afișează o listă ce conține numerele pare din lista ordonată (folosind slice)
+
+# even numbers listed ascendently using slice (print only even numbers)
 my_list.sort()
 print(my_list[1::2])
-# afișează o listă ce conține numerele impare din lista ordonată (folosind slice)
+
+# a list that contains uneven numbers (using the slice method)
 my_list.sort()
 print(my_list[::2])
-# afisează o listă ce conține numerele ce sunt multipli ai numărului 3 (folosind slice).
+
+# multiple of 3 using slice
 print(my_list[2::3])
-# a se păstra acuratețea indexilor - aceștia trebuie să fie cât mai specifici.
+
+# print the index and element values of a list
 my_list = [7,8,9,2,3,1,4,10,5,6]
 for i, e in enumerate(my_list):
     print(i,e)
